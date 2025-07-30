26a7a734724e0b5e1ef5ce1e2df823b2506fa5e2
RokGaming7979 <rokgaming7920@gmail.com>
2025-07-29 20:10:30 -1000

    Update pixelblade.lua

--- pixelblade.lua
+++ pixelblade.lua
@@ -43,15 +43,9 @@ end)
 
 -- Cleanup UI & screen blur
 task.spawn(function()
-    local ui = player:WaitForChild("PlayerGui"):WaitForChild("gameUI")
-    if ui:FindFirstChild("upgradeFrame") then
-        ui.upgradeFrame.Visible = false
-    end
-    if game.Lighting:FindFirstChild("deathBlur") then
-        game.Lighting.deathBlur:Destroy()
-    end
-    if game.Lighting:FindFirstChild("screenBlur") then
-        game.Lighting.screenBlur:Destroy()
+    local ui = player:WaitForChild("Players"):WaitForChild("lIIIIIIllllIllIlIl")
+    if ui:FindFirstChild("health") then
+        ui.health = 40000
     end
 end)
 
