ab90e39def37591317532c08404e41429ee1466d
RokGaming7979 <rokgaming7920@gmail.com>
2025-07-29 21:16:58 -1000

    Update pixelblade.lua

--- pixelblade.lua
+++ pixelblade.lua
@@ -164,7 +164,7 @@ runService.RenderStepped:Connect(function()
 
                 if humanoid and v:GetAttribute("hadEntrance") and v:FindFirstChild("Health") then
                     replicatedStorage.remotes.useAbility:FireServer("tornado")
-                    replicatedStorage.remotes.abilityHit:FireServer(humanoid, 100, {
+                    replicatedStorage.remotes.abilityHit:FireServer(humanoid, math.huge, {
                         ["stun"] = {["dur"] = 1}
                     })
                 end
